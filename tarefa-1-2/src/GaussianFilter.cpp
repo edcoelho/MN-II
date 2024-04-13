c6ea9c7aeddf064492a44c27652ccea825036bf8
Edson Coelho <edcoelho.dev@gmail.com>
2024-04-12 23:09:37 -0300

    Tarefa 1.2: Melhora os kernels dos filtros gaussinano e laplaciano.

--- tarefa-1-2/src/GaussianFilter.cpp
+++ tarefa-1-2/src/GaussianFilter.cpp
@@ -1,3 +1,3 @@
 #include "GaussianFilter.hpp"
 
-metII::GaussianFilter::GaussianFilter () : metII::Filter(kernel3{{{0.4, 0.6, 0.4}, {0.6, 1.0, 0.6}, {0.4, 0.6, 0.4}}}) {}
+metII::GaussianFilter::GaussianFilter () : metII::Filter(metII::kernel3{{{1.0f/16.0f, 2.0f/16.0f, 1.0f/16.0f}, {2.0f/16.0f, 4.0f/16.0f, 2.0f/16.0f}, {1.0f/16.0f, 2.0f/16.0f, 1.0f/16.0f}}}) {}
