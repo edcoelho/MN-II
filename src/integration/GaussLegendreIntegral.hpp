9e53906626bf04fbda615e4ea9feccfe5aafcc71
israel <israelnicolas940@gmail.com>
2024-06-13 22:01:16 -0300

    Gauss Legendre implementacao

--- src/integration/GaussLegendreIntegral.hpp
+++ src/integration/GaussLegendreIntegral.hpp
@@ -11,6 +11,15 @@ namespace metII {
 
             GaussLegendreIntegral (double _lower_limit, double _upper_limit, std::size_t num_of_gauss_points = 2, bool _use_partitions = true, double _epsilon = 1e-6);
 
+            double integrate_interval (std::function<double(double)> func, double a, double b);
+
+            double get_gauss_points() const; 
+
+            void set_gauss_points(double num_of_gauss_points); 
+
+        private: 
+
+            int gauss_points; 
     };
 
 }
